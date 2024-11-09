720fdb4de6a41be3477bf473d014607a9d6eb7eb
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-11-09 10:48:31 -0800

    Initial slideshow feature code

---
+++ src/pages/MainPage/Slideshow.jsx
@@ -0,0 +1,62 @@
+// Slideshow.jsx
+import React, { useState } from "react";
+import "./Slideshow.css";
+
+const Slideshow = () => {
+  // List of images and captions to display in the slideshow
+  const slides = [
+    {
+      image: "/images/slide1.jpg",
+      caption:
+        "Mini information about image and other things related and correlating to the image",
+    },
+    {
+      image: "/images/slide2.jpg",
+      caption: "Another slide with some description here.",
+    },
+    {
+      image: "/images/slide3.jpg",
+      caption: "More details about another event or council meeting.",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToNextSlide = () => {
+    setCurrentIndex((currentIndex + 1) % slides.length);
+  };
+
+  const goToPrevSlide = () => {
+    setCurrentIndex((currentIndex - 1 + slides.length) % slides.length);
+  };
+
+  return (
+    <div className="slideshow-container">
+      <button className="arrow left-arrow" onClick={goToPrevSlide}>
+        &lt;
+      </button>
+      <div className="slide">
+        <img
+          src={slides[currentIndex].image}
+          alt={`Slide ${currentIndex + 1}`}
+          className="slide-image"
+        />
+        <p className="caption">{slides[currentIndex].caption}</p>
+      </div>
+      <button className="arrow right-arrow" onClick={goToNextSlide}>
+        &gt;
+      </button>
+      <div className="dots">
+        {slides.map((_, index) => (
+          <span
+            key={index}
+            className={`dot ${index === currentIndex ? "active" : ""}`}
+            onClick={() => setCurrentIndex(index)}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slideshow;
