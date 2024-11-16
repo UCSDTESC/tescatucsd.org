5ee0ba8f8dbe68915add620108c1d6d34bed253d
Hieu <hieun201104@gmail.com>
2024-11-16 11:38:39 -0800

    commented out homepage events

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<NavBar />} />
         </Routes>
       </Router>
-      <HomePageEvents/>
+      {/* <HomePageEvents/> */}
       <Footer />
     </>
   );
