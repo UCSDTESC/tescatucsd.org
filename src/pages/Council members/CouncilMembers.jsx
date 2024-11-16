76e89f44cfc27720618c6deb4663e46630e60a69
Hieu <hieun201104@gmail.com>
2024-11-16 13:13:29 -0800

    Feat: Airtable

--- src/pages/Council members/CouncilMembers.jsx
+++ src/pages/Council members/CouncilMembers.jsx
@@ -1,15 +1,23 @@
 // import AirTable from "./AirTable";
 import "./CouncilMembers.css";
+const AIRTABLEURL =
+  "https://airtable.com/embed/appA3VErzwydr9T1M/shrxLuF6J25c0Z5WJ?backgroundColor=orange&viewControls=on?backgroundColor=green";
 const CouncilMembers = () => {
   return (
     <div className="">
       <div className="button-container">
         <button>
-          <a href="" className="join-button-text">
+          <a href="link" className="join-button-text">
             JOIN TESC AS AN ORGANIZATION
           </a>
         </button>
       </div>
+      <iframe
+        src={AIRTABLEURL}
+        width={1000}
+        height={1000}
+        title="EventsAirtable"
+      ></iframe>
     </div>
   );
 };
