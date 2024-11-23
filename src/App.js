0cfd566eb3d332e30932eca03fee28aea25b2280
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-11-23 11:13:23 -0800

    Code cleanup for main page demo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
 import Events from "./pages/Events/Events";
-
-import Slideshow from "./pages/MainPage/Slideshow";
+import Main from "./pages/MainPage/Main";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CouncilMembers from "./pages/Council members/Components/CouncilMembers";
+import CouncilMembers from "./pages/CouncilMembersPage/Components/CouncilMembers";
 import Page from "./components/Page";
-import HomePageEvents from "./components/HomePageEvents";
-import Sponsors from "./pages/MainPage/Sponsors";
 
 function App() {
   return (
@@ -17,18 +12,12 @@ function App() {
       <Router>
         <Routes>
           <Route element={<Page />}>
-            <Route path="/" />
+            <Route path="/" element={<Main />} />
             <Route path="/events" element={<Events />} />
             <Route path="/council-members" element={<CouncilMembers />} />
           </Route>
         </Routes>
       </Router>
-      {/* <Slideshow />
-      <HomePageEvents />
-      <Sponsors />
-      <Footer /> */}
-      {/* <Events /> */}
-      {/* <Footer /> */}
     </>
   );
 }
--- src/pages/MainPage/HomePageEvents.jsx
+++ src/pages/MainPage/HomePageEvents.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./HomePageEvents.css";
 
 const HomePageEvents = () => {
   const EventsSection = (
