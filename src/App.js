1d354da762cff12f8c59ca2a6049734188bb7aad
nselvakumar25 <nselvakumar@ucsd.edu>
2024-11-23 13:02:25 -0800

    create about us page files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Events from "./pages/Events/Events";
 import Main from "./pages/MainPage/Main";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CouncilMembers from "./pages/CouncilMembersPage/CouncilMembers";
+import AboutUs from "./pages/AboutUs/AboutUs";
 import Page from "./components/Page";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path="/events" element={<Events />} />
             <Route path="/council-members" element={<CouncilMembers />} />
             <Route path="/events" element={<Events />} />
+            <Route path="/about-us" element={<AboutUs />} />
           </Route>
         </Routes>
       </Router>
