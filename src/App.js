5f22f265f345922732ff47108afcc971c83e9094
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-11-23 12:07:31 -0800

    Further cleanup and import changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Events from "./pages/Events/Events";
 import Main from "./pages/MainPage/Main";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CouncilMembers from "./pages/CouncilMembersPage/Components/CouncilMembers";
+import CouncilMembers from "./pages/CouncilMembersPage/CouncilMembers";
 import Page from "./components/Page";
 
 function App() {
