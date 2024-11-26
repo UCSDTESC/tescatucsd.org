d92d67a60eb4ae0fba74192e9012b5500278fa80
Hieu <hieun201104@gmail.com>
2024-11-25 20:21:21 -0800

    Feat: Sponsors grid reformatting

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -4,7 +4,7 @@ import Slideshow from "./Slideshow";
 import HomePageEvents from "./HomePageEvents";
 import Sponsors from "./Sponsors";
 const divider_1_text =
-  "The Triton Engineering Student Council (TESC) empowers UC San Diego \n engineering students by connecting them with impactful communities,projects,\n and career building opportunities.";
+  "The Triton Engineering Student Council (TESC) empowers UC San Diego \n engineering students by connecting them with impactful communities, projects,\n and career building opportunities.";
 const divider_2_text =
   "In TESC we believe that best way to improve and provide the best services for the \n student body is by being accepting off all people and expanding our team to a \n wide range of people. Click here if you would like to join us!` ";
 const Main = () => {
