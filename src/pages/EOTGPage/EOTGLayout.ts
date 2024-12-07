0c593c5de43744d3b270844c03e197fccf384cf1
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-12-07 12:11:51 -0800

    EOTG Layout here

---
+++ src/pages/EOTGPage/EOTGLayout.ts
@@ -0,0 +1,15 @@
+import React from "react";
+
+class EOTGLayout extends React.Component {
+  render() {
+    return (
+      <>
+        <div id="eotg-main">
+          {React.cloneElement(this.props.children, { ...this.props })}
+        </div>
+      </>
+    );
+  }
+}
+
+export default EOTGLayout;
