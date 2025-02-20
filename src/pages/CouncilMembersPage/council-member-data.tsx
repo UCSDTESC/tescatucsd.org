17fae2101a98afdccd1cae16c541b6f781f5a002
n1sh1thaS <nishi2919@gmail.com>
2025-02-20 14:38:14 -0800

    move council member data to tsx file

---
+++ src/pages/CouncilMembersPage/council-member-data.tsx
@@ -0,0 +1,247 @@
+export const councilMemberData = [
+  {
+    Name: "American Institute of Aeronautics and Astronautics",
+    OrgAcronym: "AIAA",
+    Links: ["http://aiaaucsd.com/", "ucsdaiaa"],
+  },
+  {
+    Name: "American Institute of Chemical Engineers",
+    OrgAcronym: "AIChE",
+    Links: ["https://www.aiche.ucsd.edu/", "aicheucsd"],
+  },
+  {
+    Name: "Association for Computing Machinery",
+    OrgAcronym: "ACM",
+    Links: ["https://acmucsd.com", "acm.ucsd"],
+  },
+  {
+    Name: "Biomedical Engineering Society",
+    OrgAcronym: "BMES",
+    Links: ["https://bmes.ucsd.edu/", "ucsd_bmes"],
+  },
+  {
+    Name: "Chem-E-Car at UCSD",
+    OrgAcronym: "CEC",
+    Links: ["https://sites.google.com/a/ucsd.edu/chemecar", "chemecar_ucsd"],
+  },
+  {
+    Name: "Chinese Engineering Society",
+    OrgAcronym: "CES",
+    Links: ["https://ucsdces.com", "ucsdces"],
+  },
+  {
+    Name: "Computer Science & Engineering Society",
+    OrgAcronym: "CSES",
+    Links: ["https://csesucsd.com", "cses_ucsd"],
+  },
+  {
+    Name: "CS foreach",
+    OrgAcronym: "CSFE",
+    Links: ["csforeach.org", "csforeach"],
+  },
+  {
+    Name: "Data Science Society",
+    OrgAcronym: "DS3",
+    Links: ["https://www.ds3ucsd.com", "ds3.ucsd"],
+  },
+  {
+    Name: "Design/Build/Fly at UCSD",
+    OrgAcronym: "DBF",
+    Links: ["https://dbf.ucsd.edu", "ucsd_dbf"],
+  },
+  {
+    Name: "ECE Undergraduate Student Council",
+    OrgAcronym: "ECE USC",
+    Links: ["eceusc.ucsd.edu", "eceusc_ucsd"],
+  },
+  {
+    Name: "Engineering World Health",
+    OrgAcronym: "EWH",
+    Links: ["ewh.ucsd.edu", "ewh.at.ucsd"],
+  },
+  {
+    Name: "Engineers Without Borders at UCSD",
+    OrgAcronym: "EWB",
+    Links: ["https://linktr.ee/ewbucsandiego", "ewb_ucsandiego"],
+  },
+  {
+    Name: "EnVisionaries",
+    OrgAcronym: "EnV",
+    Links: ["https://linktr.ee/Envisionaries", ""],
+  },
+  {
+    Name: "Eta Kappa Nu",
+    OrgAcronym: "HKN",
+    Links: ["hkn.ucsd.edu", "hkn_ucsd"],
+  },
+  {
+    Name: "Gender Minorities in Bioengineering",
+    OrgAcronym: "GMBE",
+    Links: ["gmbe.ucsd.edu", "gmbe.ucsd"],
+  },
+  {
+    Name: "Human Powered Submarine at UC San Diego",
+    OrgAcronym: "HPS",
+    Links: ["https://hps-ucsd-2020.github.io/Home.html", "hps_ucsandiego"],
+  },
+  {
+    Name: "Institute of Electrical and Electronics Engineers",
+    OrgAcronym: "IEEE",
+    Links: ["https://ieeeucsd.org/", "ieee.ucsd"],
+  },
+  {
+    Name: "International Society for Pharmaceutical Engineering",
+    OrgAcronym: "ISPE",
+    Links: ["https://ispeucsd.weebly.com/", "ispeucsd"],
+  },
+  {
+    Name: "KK6UC Amateur Radio Club at UC San Diego",
+    OrgAcronym: "KK6UC",
+    Links: ["https://kk6uc.ucsd.edu/", "kk6uc"],
+  },
+  {
+    Name: "NanoEngineering & Technology Society",
+    OrgAcronym: "NETS",
+    Links: ["https://netsucsd.weebly.com/", "netsucsd"],
+  },
+  {
+    Name: "National Society of Black Engineers\nat UC San Diego",
+    OrgAcronym: "NSBE",
+    Links: ["https://nsbe.ucsd.edu/", "nsbeucsd"],
+  },
+  {
+    Name: "out in Science, Technology, Engineering and Mathematics",
+    OrgAcronym: "oSTEM",
+    Links: ["https://ostem.ucsd.edu/", "ostemucsd"],
+  },
+  {
+    Name: "Project in a Box",
+    OrgAcronym: "PiB",
+    Links: ["https://pib.ucsd.edu/", "pibucsd"],
+  },
+  {
+    Name: "Rocket Propulsion Laboratory",
+    OrgAcronym: "RPL",
+    Links: ["https://www.rocketproplab.org/", "rpl.ucsd"],
+  },
+  {
+    Name: "Society of Asian Scientists and Engineers",
+    OrgAcronym: "SASE",
+    Links: ["https://saseatucsd.weebly.com/about-us.html", "saseucsd"],
+  },
+  {
+    Name: "Society of Automotive Engineers at UC San Diego",
+    OrgAcronym: "SAE",
+    Links: ["sae.eng.ucsd.edu", "tritonracing"],
+  },
+  {
+    Name: "Society of Civil and Structural Engineers",
+    OrgAcronym: "SCSE",
+    Links: ["https://scse.ucsd.edu/home.html", "scse.ucsd"],
+  },
+  {
+    Name: "Society of Hispanic Professional Engineers at UC San Diego",
+    OrgAcronym: "SHPE",
+    Links: ["https://sites.google.com/view/shpeucsd/home", "shpeatucsd"],
+  },
+  {
+    Name: "Society of Women Engineers",
+    OrgAcronym: "SWE",
+    Links: ["https://swe-ucsd.netlify.app/", "swe.ucsd"],
+  },
+  {
+    Name: "Student Foundation Investment Committee UCSD",
+    OrgAcronym: "SFIC",
+    Links: ["https://ucsdsfic.weebly.com/about.html", "sfic_ucsd"],
+  },
+  {
+    Name: "Students for the Exploration and Development of Space",
+    OrgAcronym: "SEDS",
+    Links: ["https://www.sedsucsd.org/", "seds_ucsd"],
+  },
+  {
+    Name: "Tau Beta Pi",
+    OrgAcronym: "TBP",
+    Links: ["https://tbp.ucsd.edu/home", "tbpucsd"],
+  },
+  {
+    Name: "Triangle",
+    OrgAcronym: "Triangle",
+    Links: ["https://linktr.ee/UCSDTriangle", "triangle_ucsd"],
+  },
+  {
+    Name: "Triton AI",
+    OrgAcronym: "Triton AI",
+    Links: ["https://triton-ai.info/", "triton.ai"],
+  },
+  {
+    Name: "Triton BAJA",
+    OrgAcronym: "Triton BAJA",
+    Links: ["https://sites.google.com/ucsd.edu/tritonbaja", "tritonbaja"],
+  },
+  {
+    Name: "Triton Droids",
+    OrgAcronym: "Triton Droids",
+    Links: ["https://www.tritondroids.com/", "tritondroids"],
+  },
+  {
+    Name: "Triton NeuroTech",
+    OrgAcronym: "TNT",
+    Links: ["https://neurotech.ucsd.edu/", "tntucsd"],
+  },
+  {
+    Name: "Triton Quantitative Trading",
+    OrgAcronym: "TQT",
+    Links: ["https://tquantt.ucsd.edu/", "tqt_ucsd"],
+  },
+  {
+    Name: "Triton Racing",
+    OrgAcronym: "Triton Racing",
+    Links: ["sae.eng.ucsd.edu", "tritonracing"],
+  },
+  {
+    Name: "Triton Robosub",
+    OrgAcronym: "RBSB",
+    Links: ["https://robosub.ucsd.edu", "tritonrobosub"],
+  },
+  {
+    Name: "Triton Robotics",
+    OrgAcronym: "TR",
+    Links: ["https://tritonrobotics.ucsd.edu", "tritonrobotics"],
+  },
+  {
+    Name: "Triton Software Engineering",
+    OrgAcronym: "TSE",
+    Links: ["https://tse.ucsd.edu/", "ucsd_tse"],
+  },
+  {
+    Name: "Triton Solar Car",
+    OrgAcronym: "TSC",
+    Links: ["https://ucsdsolarcar.org/", "ucsdsolarcar/"],
+  },
+  {
+    Name: "Triton Unmanned Aerial Systems",
+    OrgAcronym: "TUAS",
+    Links: ["https://tuas.ucsd.edu/", ""],
+  },
+  {
+    Name: "Vertical Flight Society @ UC San Diego",
+    OrgAcronym: "VFS",
+    Links: ["", "vfs.ucsd"],
+  },
+  {
+    Name: "Women in Computing",
+    OrgAcronym: "WIC",
+    Links: ["https://wic.ucsd.edu/", "wicucsd"],
+  },
+  {
+    Name: "Yonder Deep",
+    OrgAcronym: "Yonder Deep",
+    Links: ["https://yonderdeep.ucsd.edu/", "yonderdeep"],
+  },
+  {
+    Name: "Yonder Dynamics",
+    OrgAcronym: "YNDR",
+    Links: ["https://yonderdynamics.org/", "yonderdynamics"],
+  },
+];
