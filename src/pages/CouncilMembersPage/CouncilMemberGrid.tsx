e9075b9906d99ea7a99510cd953d68ac4d258ad9
n1sh1thaS <nishi2919@gmail.com>
2025-02-24 22:15:10 -0800

    move icon positions and use new package

--- src/pages/CouncilMembersPage/CouncilMemberGrid.tsx
+++ src/pages/CouncilMembersPage/CouncilMemberGrid.tsx
@@ -2,7 +2,7 @@ import { councilMemberData } from "./council-member-data";
 import useImagePreloader from "../../Hooks/useImagePreload";
 import LoadingIcon from "../MainPage/LoadingIcon";
 import { TbWorldWww } from "react-icons/tb";
-import { FaInstagram } from "react-icons/fa";
+import { InstagramOutlined, GlobalOutlined } from "@ant-design/icons";
 
 export default function CouncilMemberGrid() {
   const imagePreloader = useImagePreloader(
@@ -16,7 +16,7 @@ export default function CouncilMemberGrid() {
         councilMemberData.map((councilMember, index) => (
           <div
             key={index}
-            className="w-72 h-88 bg-white shadow-2xl rounded-standard p-3 mx-4"
+            className="relative w-72 h-88 bg-white shadow-2xl rounded-standard p-3 mx-4"
           >
             <div
               key={index}
@@ -30,12 +30,12 @@ export default function CouncilMemberGrid() {
             </div>
             <div key={councilMember.OrgAcronym} className="p-2 flex flex-col">
               <p className="text-center font-semibold">{councilMember.Name}</p>
-              <div className="flex flex-row justify-center items-center gap-3">
+              <div className="absolute bottom-3 right-3 flex flex-row justify-center items-center gap-3">
                 <a href={councilMember.Links[0]}>
-                  <TbWorldWww size={22} />
+                  <GlobalOutlined className="text-[1.5vw]" />
                 </a>
                 <a href={`https://www.instagram.com/${councilMember.Links[1]}`}>
-                  <FaInstagram size={22} />
+                  <InstagramOutlined className="text-[1.5vw]" />
                 </a>
               </div>
             </div>
