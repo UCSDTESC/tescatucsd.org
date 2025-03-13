3463ecae58056d34aa4a0af2eb038a44640c2699
n1sh1thaS <nishi2919@gmail.com>
2025-03-13 01:02:07 -0700

    about us header

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -18,17 +18,19 @@ const AboutUs = () => {
           fetchPriority="high"
         />
       </Helmet>
-      <img
-        className="w-full h-[50vh] object-cover"
-        src={eventsPageImage}
-        alt=""
-        fetchPriority="high"
-      ></img>
-      <Divider>
-        The Triton Engineering Student Council (TESC), empowers UC San Diego's
-        <br /> engineering students by connecting them to impactful communities,
-        <br /> projects, and career-building opportunities
-      </Divider>
+      <div className="m-12 mt-20 flex flex-row justify-between items-center max-w-[80%] mx-auto">
+        <div className="max-w-3/5">
+          <p className="font-semibold text-[60px] text-[#11426B] leading-14 mb-3">
+            Get to know our <br /> board members
+          </p>
+          <p className="font-semibold text-[22px]">
+            Meet the leaders behind our organization! Our board members are
+            dedicated to fostering community, innovation, and growth. Get to
+            know the team working to bring our mission to life!
+          </p>
+        </div>
+        <img src="AllPages/bear.png" alt="bear" className="w-72 h-auto"></img>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-4 gap-5 mx-auto w-[80%]">
         <Suspense fallback={<LoadingIcon />}>
           <AboutUsContent />
