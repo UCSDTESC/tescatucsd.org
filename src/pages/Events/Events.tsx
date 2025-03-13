615171fd421609c14d13a303bc9419547c1963c2
n1sh1thaS <nishi2919@gmail.com>
2025-03-13 02:32:04 -0700

    create header

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -20,18 +20,25 @@ const Events = () => {
         />
       </Helmet>
 
-      <img
-        className="w-full h-[50vh] object-cover"
-        src={eventsPageImage}
-        alt=""
-        fetchPriority="high"
-      />
-      <Divider>
-        <p>
-          At TESC, we believe events are the best way to grow and <br /> enhance
-          the engineering student body
-        </p>
-      </Divider>
+      <div className="m-12 mt-20 flex flex-row justify-between items-center max-w-[80%] mx-auto">
+        <div className="sm:max-w-full md:max-w-3/5 ">
+          <p className="font-semibold text-[40px] text-[#11426B] leading-10 mb-3">
+            Jump into engineering leadership—where ideas comes alive and
+            students shape what’s next...
+          </p>
+          <p className="font-semibold text-[22px] mb-3">
+            Our events bring together passionate engineers to learn, connect,
+            and lead. Whether it's talk by start-ups, interactive workshop, or
+            socials, there's always an opportunity to engage, grow, and make an
+            impact!
+          </p>
+        </div>
+        <img
+          src="AllPages/events-lead.png"
+          alt="lead"
+          className="hidden md:block w-80 h-auto"
+        ></img>
+      </div>
 
       <div className="mt-4 mx-auto w-[70%]">
         <h1 className="font-medium [line-height:1.2] text-[2vw] mb-2">
