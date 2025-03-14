34c4f43ae01d63ffb04f1cfe52a77d0e3556c299
n1sh1thaS <nishi2919@gmail.com>
2025-03-13 17:15:53 -0700

    edit event cards

--- src/pages/MainPage/Sponsors.tsx
+++ src/pages/MainPage/Sponsors.tsx
@@ -7,7 +7,7 @@ export default function Sponsors() {
   return (
     <div className="mb-[7rem] flex justify-center">
       <div className="w-[70%]">
-        <h1 className="text-[30px] text-[#11426B] font-bold mb-4">Sponsors</h1>
+        <h1 className="text-[30px] text-[#003059] font-bold mb-4">Sponsors</h1>
 
         <div className="grid gap-[1rem] grid-cols-3 justify-center">
           <Suspense fallback={<LoadingIcon />}>
