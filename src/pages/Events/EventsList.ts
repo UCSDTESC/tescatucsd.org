59c7b512b50e81197b3dfa9bdb83acda65db6bba
B2Bomber2 <ferrariguan@HP-Laptop-15-MacOS.local>
2025-04-02 19:28:20 -0700

    updated the events again

--- src/pages/Events/EventsList.ts
+++ src/pages/Events/EventsList.ts
@@ -1,7 +1,11 @@
-export const upcomingEventLogos = ["/event-logos/Tapioca.png"];
-export const upcomingEventLinks = ["https://tapiocaexpress.com/"];
+export const upcomingEventLogos = ["/event-logos/field_day_2.png"];
+export const upcomingEventLinks = ["https://tescucsd.org"];
+export const upcomingEventDescription = [
+  "Come out the Matthew's Quad and enjoys some games like 'Tug of War' and 'Potato Sack Race'!"
+];
 
 export const pastEventLogos = [
+  "/event-logos/Tapioca.png",
   "/event-logos/SandcastleBonfire.jpg",
   "/event-logos/stempad-talk-wi25.webp",
   "/event-logos/decaf-event-wi25.png",
@@ -19,6 +23,7 @@ export const pastEventLogos = [
 ];
 
 export const pastEventLinks = [
+  "https://tapiocaexpress.com/",
   "https://docs.google.com/forms/d/e/1FAIpQLSfYP_vwKSgp0AF6B310_VyWz5hGEI3Hg6UfZetKdhE_THPiNA/viewform",
   "http://tinyurl.com/stempad-rsvp",
   "https://decafucsd.com",
@@ -35,11 +40,8 @@ export const pastEventLinks = [
   "https://www.eotgucsd.com"
 ];
 
-export const upcomingEventDescription = [
-  "Come out and support the Triton Engineering Student Council (TESC) by purchasing from Tapioca Express! Show this flyer at the register and 10% of the proceeds will go to TESC."
-];
-
 export const pastEventDescription = [
+  "Come out and support the Triton Engineering Student Council (TESC) by purchasing from Tapioca Express! Show this flyer at the register and 10% of the proceeds will go to TESC.",
   "Triton Engineering Student Council is inviting you to participate in our Sand Castle Competition and join us for a warm and cozy bonfire with some smores' on March 2nd, 2025! We are opening this event to all students within the Jacobs School of Engineering community! For this Sand Castle Competition, the best sand castles will be given points. We will be mostly judging you on your team spirit, quality of the sand castle, and engineering aspects when building it! Afterwards, we will be celebrating the winners with some smore's and a bonfire!!", 
   "On Friday, February 7th, from 1:00-3:00 PM, TESC will be hosting a talk with Stempad at the QCC in Jacob's Hall. Stempad is a digital scientific note editor startup and they will be talking about how to build a startup and the difference between a startup and a large company.",
   "DECaF 2025, a student-run career fair organized by the Triton Engineering Student Council, aims to connect talented engineering students with leading recruiters like you. With an annual attendance of 1,000+ students from various engineering majors, this event presents a unique opportunity to meet and engage with potential future employees. Participating companies will have direct access to a diverse pool of motivated and skilled students and a comprehensive resume database."
