c3c061b5b2fe5c7552d6f2a189dc7695922b45c1
B2Bomber2 <ferrariguan@HP-Laptop-15-MacOS.local>
2025-04-19 12:00:51 -0700

    added asml event

--- src/pages/Events/EventsList.ts
+++ src/pages/Events/EventsList.ts
@@ -1,10 +1,12 @@
-export const upcomingEventLogos = ["/event-logos/field_day_2.jpg"];
-export const upcomingEventLinks = ["https://docs.google.com/forms/d/1K9-IcUQsjNAHR9n5aFxCHzhuaVX2rKbU1-25iVOeKQA/viewform?edit_requested=true"];
+export const upcomingEventLogos = ["/event-logos/asml.jpg"];
+export const upcomingEventLinks = ["https://docs.google.com/forms/d/e/1FAIpQLScuAw4QWprCEHYHNvPYP8WyVq-MAL1og0M48x2bXnw2Swm59A/viewform"];
 export const upcomingEventDescription = [
-  "Hello participants! Ready to put your skills to the test and show if you can take on the competition? Sign up now for the TESC Olympics event, starting on April 19th from 10am to 2pm on the Matthews Quad. We'll have a variety of activities for all to enjoy, so please RSVP to indicate which activities you will participate in. Winners of the games will be awarded trophies as well! We'll be providing snacks and lunch, so if you have any dietary restrictions, please list them below. Hope to see you there! And may the odds be in your favor...."
+  "Join us for an exciting tech talk about the EUV Lithography process hosted by ASML at the Qualcomm Conference Center - JSOE 1st Floor on Fri., April 25, 2025, from 10:00 AM to 12:00 PM. In addition to the technical discussion, ASML will also share valuable information about their upcoming internship and co-op opportunities—a great chance for students interested in cutting-edge semiconductor technology so please be sure to bring your resumes (soft copy)!"
 ];
 
 export const pastEventLogos = [
+  "/event-logos/field_day_2.png",
+  "/event-logos/Tapioca.png",
   "/event-logos/Tapioca.png",
   "/event-logos/SandcastleBonfire.jpg",
   "/event-logos/stempad-talk-wi25.webp",
@@ -23,6 +25,8 @@ export const pastEventLogos = [
 ];
 
 export const pastEventLinks = [
+  "https://tescucsd.org",
+  "https://tapiocaexpress.com/",
   "https://tapiocaexpress.com/",
   "https://docs.google.com/forms/d/e/1FAIpQLSfYP_vwKSgp0AF6B310_VyWz5hGEI3Hg6UfZetKdhE_THPiNA/viewform",
   "http://tinyurl.com/stempad-rsvp",
@@ -41,6 +45,8 @@ export const pastEventLinks = [
 ];
 
 export const pastEventDescription = [
+  "Come out the Matthew's Quad and enjoys some games like 'Tug of War' and 'Potato Sack Race'!",
+  "Come out and support the Triton Engineering Student Council (TESC) by purchasing from Tapioca Express! Show this flyer at the register and 10% of the proceeds will go to TESC.",
   "Come out and support the Triton Engineering Student Council (TESC) by purchasing from Tapioca Express! Show this flyer at the register and 10% of the proceeds will go to TESC.",
   "Triton Engineering Student Council is inviting you to participate in our Sand Castle Competition and join us for a warm and cozy bonfire with some smores' on March 2nd, 2025! We are opening this event to all students within the Jacobs School of Engineering community! For this Sand Castle Competition, the best sand castles will be given points. We will be mostly judging you on your team spirit, quality of the sand castle, and engineering aspects when building it! Afterwards, we will be celebrating the winners with some smore's and a bonfire!!", 
   "On Friday, February 7th, from 1:00-3:00 PM, TESC will be hosting a talk with Stempad at the QCC in Jacob's Hall. Stempad is a digital scientific note editor startup and they will be talking about how to build a startup and the difference between a startup and a large company.",
