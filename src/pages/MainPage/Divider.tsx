61e170da5e8bfd856482a3703edbb91767582c8f
B2Bomber2 <ferrariguan@HP-Laptop-15-MacOS.local>
2025-05-14 19:19:36 -0700

    enlarged the font

--- src/pages/MainPage/Divider.tsx
+++ src/pages/MainPage/Divider.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Divider({ children }: { children: ReactNode }) {
   return (
     <div className="flex justify-center items-center min-h-[26vh] mb-[5vh] text-white p-5 bg-radial from-[#114675] from-40% to-navy">
-      <p className="max-w-[50%] text-center m-0 text-[1.1vw] leading-[1.5] font-medium whitespace-normal">
+      <p className="max-w-[60%] text-center m-0 leading-[1.5] font-xl whitespace-normal">
         {children}
       </p>
     </div>
