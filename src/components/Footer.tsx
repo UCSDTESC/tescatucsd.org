a386f4355141fb69c8fdb83d65e9d6d7af75039c
Thanh Trinh <thanhnghiep1007@gmail.com>
2025-05-17 11:52:17 -0700

    redesign footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,60 +1,9 @@
-import { NavLink } from "react-router-dom";
-// import "./footer.css";
-import {
-  DiscordOutlined,
-  InstagramOutlined,
-  LinkedinOutlined,
-  MailOutlined,
-  TwitterOutlined,
-} from "@ant-design/icons";
 import instagramIcon from "../../public/social-icons/Instagram.png"
 import discordIcon from "../../public/social-icons/Discord.png"
 import mailIcon from "../../public/social-icons/mail.png"
 import linkedInIcon from "../../public/social-icons/LinkedIn.png"
 import twitterIcon from "../../public/social-icons/Twitter.png"
 const Footer = () => {
-  // const pages = (
-  //   <ul className="flex">
-  //     <li className="text-[26px] font-medium list-none text-center px-4">
-  //       <NavLink className="nav-link" to="/about-us">
-  //         <span className="nav-text">About Us </span>
-  //       </NavLink>
-  //     </li>
-  //     <li className="text-[26px] font-medium list-none text-center px-4">
-  //       <NavLink className="nav-link" to="/events">
-  //         <span className="nav-text">Events</span>
-  //       </NavLink>
-  //     </li>
-  //     <li className="text-[26px] font-medium list-none  text-center px-4">
-  //       <NavLink className="nav-link" to="/council-members">
-  //         <span className="nav-text">Council Members</span>
-  //       </NavLink>
-  //     </li>
-  //   </ul>
-  // );
-  // const pages = (
-  //   <div className="xl:flex items-center hidden">
-  //     <NavLink
-  //       className="h-min mx-[32px]"
-  //       to="https://tesc.typeform.com/to/hlC07HII"
-  //     >
-  //       <span className="text-offWhite text-[26px] font-medium">Join Us</span>
-  //     </NavLink>
-  //     <NavLink className="h-min mx-[32px]" to="/about-us">
-  //       <span className="text-offWhite text-[26px] font-medium">About Us </span>
-  //     </NavLink>
-
-  //     <NavLink className="h-min mx-[32px]" to="/events">
-  //       <span className="text-offWhite text-[26px] font-medium">Events</span>
-  //     </NavLink>
-
-  //     <NavLink className="h-min mx-[32px]" to="/council-members">
-  //       <span className="text-offWhite text-[26px] font-medium">
-  //         Council Members
-  //       </span>
-  //     </NavLink>
-  //   </div>
-  // );
   const contactIcons = (
     <div className="flex m-0 p-0 h-[39px] items-center gap-[25px]">
       <a
@@ -111,14 +60,16 @@ const Footer = () => {
 
   return (
     <>
-      <footer className="bg-radial from-[#114675] from-40% to-navy pl-[2%] min-h-[15vh] flex justify-between w-full items-center">
-        <div className=" xl:flex-row flex items-start text-left gap-5">
-          <p className="text-offWhite text-[26px] leading-[39px] font-medium list-none">
-            Contact Us
+      <footer className="bg-radial from-[#114675] from-40% to-navy w-full py-12 px-18 sm:px-25 md:px-34 lg:px-40 xl:px-55">
+        <div className="max-w-5xl flex flex-col items-start text-left">
+          <h2 className="text-offWhite text-[26px] font-bold mb-3">
+        Interested? Connect with us!
+          </h2>
+          <p className="text-offWhite text-[16px] font-normal max-w-md leading-relaxed mb-5">
+        Stay connected with us! Follow us for updates on events, opportunities, and ways to get involved.
           </p>
-          <div>{contactIcons}</div>
+          {contactIcons}
         </div>
-        {/* <div>{pages}</div> */}
       </footer>
     </>
   );
