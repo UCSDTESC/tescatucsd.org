02144c8e5f5edd6ba1c271475a78eb1ba1659486
Ferrari <ferrari@Razer-Blade-14-MacOS.local>
2026-01-14 13:42:04 -0800

    home page auto pulls from event page

--- src/pages/MainPage/HomePageEvents.tsx
+++ src/pages/MainPage/HomePageEvents.tsx
@@ -1,25 +1,10 @@
 import { Suspense } from "react";
 import HomePageEventCards from "./HomePageEventsCards";
+import { upcomingEvents, pastEvents } from "../Events/EventsList.ts";
 
 const events = [
-  {
-    name: "Tapioca Express Fundraiser", 
-    image: "/event-logos/tapex_fundraiser.jpg", 
-    description: "Join us this Friday at Tapioca Express to help raise funds for TESC, ISPE, BMES, and EWB! This will be a great opportunity to connect with your fellow engineering students, so be sure to pull up!",
-    link: "https://tapiocaexpress.com/"
-  },
-  {
-    name: "Board Game Night", 
-    image: "/event-logos/BoardGameNightPostWLogos.jpg", 
-    description: "Take a break from midterms, get food at Tapex, and have fun playing Codenames, Secret Hitler, Poker, Exploding Kittens, and much more! 🍽️🎲",
-    link: ""
-  },
-  {
-    name: "Engineers on the Green", 
-    image: "/event-logos/eotg-winter2026.jpg", 
-    description: "Join us at Engineers on the Green, the annual engineering club fair where you can explore exciting projects, meet fellow innovators, and find your place in our vibrant engineering community. Discover clubs, get involved, and kickstart your journey into hands-on engineering!",
-    link: "https://www.eotgatucsd.com "
-  }
+  ...upcomingEvents.slice(0, 3),
+  ...pastEvents.slice(0, Math.max(0, 3 - upcomingEvents.length))
 ];
 
 const HomePageEvents = () => {
