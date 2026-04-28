3a9d534b7d79f7a845d45633c48ee81e946f8d0c
Hieu <hieun201104@gmail.com>
2026-04-27 19:52:32 -0700

    Feat: portal in the nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -34,7 +34,7 @@ const NavBar = () => {
         <img className="h-[80px] aspect-auto" src={TescLogoWhite} alt="" />
       </NavLink>
       <button
-        className="text-white md:hidden text-[clamp(16px,3vw,40px)]"
+        className="text-white lg:hidden text-[clamp(16px,3vw,40px)]"
         ref={buttonRef}
         onClick={() => {
           setMenuOpen(!menuOpen);
@@ -42,23 +42,31 @@ const NavBar = () => {
       >
         <MenuOutlined />
       </button>
-      <div className="hidden md:flex items-center  text-[clamp(16px,2vw,26px)]">
+      <div className="hidden lg:flex items-center text-[clamp(14px,1.4vw,22px)]">
         <a
-          className="h-min mx-[32px]"
+          className="h-min mx-4 xl:mx-6 whitespace-nowrap"
           href="https://tesc.typeform.com/to/hlC07HII"
           target="_blank"
           rel="noreferrer"
         >
           <span className="text-offWhite  font-medium">Join Us</span>
         </a>
-        <NavLink className="h-min mx-[32px]" to="/about-us">
+        <NavLink className="h-min mx-4 xl:mx-6 whitespace-nowrap" to="/about-us">
           <span className="text-offWhite  font-medium">About Us </span>
         </NavLink>
 
-        <NavLink className="h-min mx-[32px]" to="/events">
+        <NavLink className="h-min mx-4 xl:mx-6 whitespace-nowrap" to="/events">
           <span className="text-offWhite  font-medium">Events</span>
         </NavLink>
-        <div className="relative group h-min mx-[32px]">
+        <a
+          className="h-min mx-4 xl:mx-6 whitespace-nowrap"
+          href="https://portal.tescatucsd.org"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="text-offWhite  font-medium">Portal</span>
+        </a>
+        <div className="relative group h-min mx-4 xl:mx-6">
           <span className="cursor-default text-offWhite font-medium select-none">
             Council Members
           </span>
@@ -80,13 +88,13 @@ const NavBar = () => {
           </div>
         </div>
 
-        <NavLink className="h-min mx-[32px]" to="/funding">
+        <NavLink className="h-min mx-4 xl:mx-6 whitespace-nowrap" to="/funding">
           <span className="text-offWhite  font-medium">Funding</span>
         </NavLink>
       </div>
 
       <div
-        className={`absolute md:hidden w-max flex flex-col top-20 z-100 bg-navy right-0 items-end text-center overflow-hidden animate-[navbar-animate_1s_forwards]
+        className={`absolute lg:hidden w-max flex flex-col top-20 z-100 bg-navy right-0 items-end text-center overflow-hidden animate-[navbar-animate_1s_forwards]
       ${menuOpen ? "block" : "hidden"}`}
         ref={dropdownRef}
       >
@@ -104,6 +112,14 @@ const NavBar = () => {
         <NavLink className="h-max py-3 mx-[20px]" to="/events">
           <span className="text-offWhite py-2 font-medium">Events</span>
         </NavLink>
+        <a
+          className="h-max py-3 mx-[20px]"
+          href="https://portal.tescatucsd.org"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="text-offWhite py-2 font-medium">Portal</span>
+        </a>
         <div className="flex flex-col items-end gap-1 py-2 mx-[20px]">
           <span className="text-offWhite font-medium">Council Members</span>
           <NavLink className="h-max py-1" to="/council-members">
@@ -113,6 +129,9 @@ const NavBar = () => {
             <span className="text-offWhite/90 text-sm font-medium">Accomplishments</span>
           </NavLink>
         </div>
+        <NavLink className="h-max py-3 mx-[20px]" to="/funding">
+          <span className="text-offWhite py-2 font-medium">Funding</span>
+        </NavLink>
       </div>
     </div>
   );
